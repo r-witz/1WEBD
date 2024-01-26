58191d78cea1d09f7b89c9bbc1a43e3c7a287712
Raphael Witz <r.witz@proton.me>
2024-01-26 21:04:17 +0100

    feat[pages-load]: infinite scrolling + search query url implemented

--- search/search-api.js
+++ search/search-api.js
@@ -1,33 +1,54 @@
-function searchApi() {
+let pageNb = 1;
+
+function searchApi(fromScroll) {
   let input = document.getElementById("inputField").value;
   input = input.replace(/\s/g, "+");
 
-  const url = `https://www.omdbapi.com/?apikey=b7685432&s=${input}`;
+  const url = `https://www.omdbapi.com/?apikey=b7685432&page=${pageNb}&s=${input}`;
 
   fetch(url)
     .then((response) => response.json())
 
     .then((data) => {
-      if (data.Response == "False") {
-        alert(data.Error);
-      } else {
+      if (data.Response == "False" && !fromScroll) {
+        document.getElementById("error").innerHTML = data.Error;
+      } else if (data.Response == "True") {
+        document.getElementById("error").innerHTML = "";
         data.Search.forEach((element) => {
           if (element.Type == "movie" && element.Poster != "N/A") {
+            let movieCard = document.createElement("a");
+            movieCard.classList.add('movieCard');
+            movieCard.href = `./movie.html?id=${element.imdbID}`;
             let movieTitle = document.createElement("h3");
             movieTitle.innerHTML = element.Title;
             let moviePoster = document.createElement("img");
             moviePoster.src = element.Poster;
-
-            document.getElementById("movieList").appendChild(movieTitle);
-            document.getElementById("movieList").appendChild(moviePoster);
+            
+            document.getElementById("movieList").appendChild(movieCard)
+            movieCard.appendChild(movieTitle);
+            movieCard.appendChild(moviePoster);
           }
         });
-      }
+      } else {console.log(data.Error)}
     });
 }
 
 document.getElementById("apiForm").addEventListener("submit", function (event) {
   event.preventDefault();
   document.getElementById("movieList").innerHTML = "";
-  searchApi();
+  searchApi(false);
 });
+
+window.onscroll = infiniteScroll;
+
+    let isExecuted = false;
+
+    function infiniteScroll() {
+        if (window.scrollY > (document.body.offsetHeight - window.outerHeight) && !isExecuted) {
+            isExecuted = true;
+            pageNb++;
+
+            searchApi(true);
+            isExecuted = false
+        }
+    }
